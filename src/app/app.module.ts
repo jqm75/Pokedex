7aff00895330c3375382b893ab3af961381c25f3
Joaquim Pujol <joaquim.pujol@gmail.com>
2023-06-17 22:11:23 +0200

    Commit Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PhotoPokemonComponent } from './components/photo-pokemon/photo-pokemon.component';
+import { CardPokemonComponent } from './components/card-pokemon/card-pokemon.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PhotoPokemonComponent,
+    CardPokemonComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card-pokemon/card-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pokemon',
+  templateUrl: './card-pokemon.component.html',
+  styleUrls: ['./card-pokemon.component.scss']
+})
+export class CardPokemonComponent {
+
+}
